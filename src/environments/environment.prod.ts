4d8e572e94e893731542e0645ed7ccf10bbd9087
Me <me@My-MacBook-Pro.mynet>
2018-04-13 01:48:56 +0300

    added components and navbar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDQabwr0X3VGv6vH0n9yjMYSec0jhDXij8',
+    authDomain: 'ng-ecommerce-mazmi.firebaseapp.com',
+    databaseURL: 'https://ng-ecommerce-mazmi.firebaseio.com',
+    projectId: 'ng-ecommerce-mazmi',
+    storageBucket: 'ng-ecommerce-mazmi.appspot.com',
+    messagingSenderId: '884108123'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDQabwr0X3VGv6vH0n9yjMYSec0jhDXij8',
+    authDomain: 'ng-ecommerce-mazmi.firebaseapp.com',
+    databaseURL: 'https://ng-ecommerce-mazmi.firebaseio.com',
+    projectId: 'ng-ecommerce-mazmi',
+    storageBucket: 'ng-ecommerce-mazmi.appspot.com',
+    messagingSenderId: '884108123'
+  }
 };
