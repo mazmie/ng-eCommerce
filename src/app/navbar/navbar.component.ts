7f12f81f6727f906880f37d49c5387234cd3edb7
Me <me@My-MacBook-Pro.mynet>
2018-05-10 13:09:49 +0300

    added cart items count badge into navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,20 +1,35 @@
+import { ShoppingCartService } from './../shopping-cart.service';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AppUser } from '../models/app-user';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
   appUser: AppUser;
+  itemsCount = 0;
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService,
+    private shoppingCartService: ShoppingCartService) {
     this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
-   }
+  }
+
+  async ngOnInit() {
+    (await this.shoppingCartService.getCart())
+      .subscribe(cart => {
+        this.itemsCount = 0;
+        if (!cart.items) { return 0; }
+        for (const productId of Object.keys(cart.items)) {
+          this.itemsCount += cart.items[productId].quantity;
+        }
+      });
+  }
 
   logout() {
     this.auth.logout();
